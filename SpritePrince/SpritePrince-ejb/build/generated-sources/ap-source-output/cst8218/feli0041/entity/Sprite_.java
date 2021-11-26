8cab19efd3d98f0f4c85df9bbd57602cdd109c3e
Prince Felix <princeafelix@gmail.com>
2021-11-26 13:52:45 -0500

    Added AppUser v1.1

--- SpritePrince/SpritePrince-ejb/build/generated-sources/ap-source-output/cst8218/feli0041/entity/Sprite_.java
+++ SpritePrince/SpritePrince-ejb/build/generated-sources/ap-source-output/cst8218/feli0041/entity/Sprite_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-11-26T13:09:24")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-11-26T13:30:49")
 @StaticMetamodel(Sprite.class)
 public class Sprite_ { 
 
